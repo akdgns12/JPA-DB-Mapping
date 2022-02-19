aeb0cae40808ba2ecdbd31ea35190d04b3a38d50
akdgns12 <akdgns12@naver.com>
2022-02-19 17:07:44 +0900

    상속매핑관계 - 단이테이블 전략

--- src/main/java/jpabook/jpashop/domain/Item.java
+++ src/main/java/jpabook/jpashop/domain/Item.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Entity
-@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 조인전략
-@DiscriminatorColumn
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE) // 조인전략 : @Inheritance(strategy = InheritanceType.JOIN)
+@DiscriminatorColumn // 단일테이블 전략에서는 이 어노테이션 없어도 DTYPE이 생성됨 - 단일테이블에 데이터들이 모여있으니 어느 테이블의 데이터인지 구분할 DTYPE이 있어야하므로
 public class Item {
 
     @Id @GeneratedValue
